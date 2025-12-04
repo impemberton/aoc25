f218a612563a91c442ffd7e140900060acf33f40
iketyke <isaac.mellad.pemberton@gmail.com>
2025-12-04 08:00:11 +0000

    complete day 2 part 1

---
+++ day2/giftshop.py
@@ -0,0 +1,14 @@
+with open("input.txt") as input:
+    input = input.read()
+
+ids = input.split(",")
+
+sum_of_ids = 0
+for id_range in ids:
+    first_id, last_id = id_range.split("-")
+    for id in range(int(first_id), int(last_id)+1):
+        string_id = str(id)
+        if string_id[:len(string_id)//2] == string_id[len(string_id)//2:]:
+            sum_of_ids += id
+
+print(sum_of_ids)
